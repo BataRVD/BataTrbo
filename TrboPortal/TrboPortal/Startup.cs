28256c7eabd81ca0063446e82259d6c162f6c427
Erik Hubers <github@erikhubers.nl>
2020-02-22 23:30:19 +0100

    Initial commit swagger stuff

--- TrboPortal/TrboPortal/Startup.cs
+++ TrboPortal/TrboPortal/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TrboPortal.Controllers;
 
 namespace TrboPortal
 {
@@ -25,15 +26,20 @@ namespace TrboPortal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerDocument();
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUi3();
             }
 
             app.UseHttpsRedirection();
