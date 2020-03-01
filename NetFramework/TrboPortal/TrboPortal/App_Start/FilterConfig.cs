2fd22d6be4c8b7d95c4c318f109c1b0690807ec5
Jeroen <Jeroen@sparkline.nl>
2020-03-01 15:39:13 +0100

    The new solution

---
+++ NetFramework/TrboPortal/TrboPortal/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TrboPortal
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
