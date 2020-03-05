c02f7e44ea725a8dcaed884dadd3435befe9f424
Erik <a@b.com>
2020-03-05 21:21:14 +0100

    Implemented GetLogging

---
+++ TrboPortal/TrboPortal/Model/Api/LogMessage.cs
@@ -0,0 +1,36 @@
+﻿using TrboPortal.Mappers;
+using TrboPortal.Model.Db;
+
+namespace TrboPortal.Model.Api
+{
+    public class LogMessage
+    {
+        [Newtonsoft.Json.JsonProperty("DateTime", Required = Newtonsoft.Json.Required.Always)]
+        [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+        public string DateTime { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("Level", Required = Newtonsoft.Json.Required.Always)]
+        public string Level { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("CallSite", Required = Newtonsoft.Json.Required.Default)]
+        [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+        public string CallSite { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("Message", Required = Newtonsoft.Json.Required.Default)]
+        [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+        public string Message { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("Exception", Required = Newtonsoft.Json.Required.AllowNull)]
+        [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+        public string Exception { get; set; }
+
+        public LogMessage(LogEntry l)
+        {
+            DateTime = DateTimeMapper.ToString(l.Timestamp);
+            Level = l.LogLevel;
+            CallSite = l.CallSite ?? string.Empty;
+            Message = l.Message ?? string.Empty;
+            Exception = l.Exception;
+        }
+    }
+}
---
+++ TrboPortal/TrboPortal/Model/Db/LogEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using TrboPortal.Mappers;
+
+namespace TrboPortal.Model.Db
+{
+    /// <summary>
+    /// Model used for database storage, when changing please look into the default migration options of entityframework
+    ///</summary>
+    public class LogEntry
+    {
+        [Key]
+        public int LogEntryId { set; get; }
+
+        public long Timestamp { get; set; }
+        public string LogLevel { get; set; }
+        public string CallSite { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+
+        public override string ToString() => $"{DateTimeMapper.ToString(Timestamp)} {LogLevel} {CallSite} {Message} {Exception}";
+    }
+}
